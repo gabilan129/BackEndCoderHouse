53e58bf1c411216e3a577ed20d2884c2eef94868
gabilan129 <gabrielraul16@gmail.com>
2023-07-01 20:56:23 -0300

    2 parte desafio1 ya verifica que los codigos no sean iguales

--- Trabajo01.js
+++ Trabajo01.js
@@ -8,14 +8,37 @@ class ProductManager {
   };
 
   addProducts = (title, description, price, thumbnail, code, stock) => {
-    const newProducto = {
-        title, description, price, thumbnail, code, stock
-    } 
-    return this.productos.push(newProducto);
+
+
+
+
+
+
+if(!title || !description || !price || !thumbnail || !code || !stock ){
+    console.error("Campos Faltantes")
+}else{
+
+    const ProductoFiltrado = this.productos.find(el =>el.code==code)
+    if(!ProductoFiltrado){
+
+        const newProducto = {
+            title, description, price, thumbnail, code, stock
+        } 
+        return this.productos.push(newProducto);
+    }else{
+        console.error("el codigo del producto ya existe")
+    }
+
   };
+}
+
+
 }
 
 
 const productoMang = new ProductManager()
 productoMang.addProducts("producto1","description1",1,"url","code1",100)
+productoMang.addProducts("producto1","description1",2,"url","code1",50)
+
+
 console.log(productoMang.getProducts())
