509d1e5822dfa6650fc448914c88c14cf764e8df
gabilan129 <gabrielraul16@gmail.com>
2023-07-01 20:35:08 -0300

    1 parte desafio1

--- Trabajo01.js
+++ Trabajo01.js
@@ -1,1 +1,21 @@
-let gabriel = "practicando github por que no me acuerdo "
+class ProductManager {
+  constructor() {
+    this.productos = [];
+  }
+
+  getProducts = () => {
+    return this.productos;
+  };
+
+  addProducts = (title, description, price, thumbnail, code, stock) => {
+    const newProducto = {
+        title, description, price, thumbnail, code, stock
+    } 
+    return this.productos.push(newProducto);
+  };
+}
+
+
+const productoMang = new ProductManager()
+productoMang.addProducts("producto1","description1",1,"url","code1",100)
+console.log(productoMang.getProducts())
