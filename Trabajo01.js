609ea342b8a7bc0eb0ae0d9710ed544981091b71
gabilan129 <gabrielraul16@gmail.com>
2023-07-01 21:23:45 -0300

    3 parte desafio1 id automaticos

--- Trabajo01.js
+++ Trabajo01.js
@@ -3,42 +3,46 @@ class ProductManager {
     this.productos = [];
   }
 
+  
   getProducts = () => {
-    return this.productos;
-  };
+      return this.productos;
+    };
 
+    generarId = () => {
+      const contar = this.productos.length;
+      if (contar == 0) {
+        return 1;
+      } else {
+        return contar + 1;
+      }
+    };
+    
   addProducts = (title, description, price, thumbnail, code, stock) => {
-
-
-
-
-
-
-if(!title || !description || !price || !thumbnail || !code || !stock ){
-    console.error("Campos Faltantes")
-}else{
-
-    const ProductoFiltrado = this.productos.find(el =>el.code==code)
-    if(!ProductoFiltrado){
-
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      console.error("Campos Faltantes");
+    } else {
+      const ProductoFiltrado = this.productos.find((el) => el.code == code);
+      const id = this.generarId();
+      if (!ProductoFiltrado) {
         const newProducto = {
-            title, description, price, thumbnail, code, stock
-        } 
+          id: id,
+          title,
+          description,
+          price,
+          thumbnail,
+          code,
+          stock,
+        };
         return this.productos.push(newProducto);
-    }else{
-        console.error("el codigo del producto ya existe")
+      } else {
+        console.error("el codigo del producto ya existe");
+      }
     }
-
   };
 }
 
+const productoMang = new ProductManager();
+productoMang.addProducts("producto1", "description1", 1, "url", "code1", 100);
+productoMang.addProducts("producto1", "description1", 2, "url", "code2", 200);
 
-}
-
-
-const productoMang = new ProductManager()
-productoMang.addProducts("producto1","description1",1,"url","code1",100)
-productoMang.addProducts("producto1","description1",2,"url","code1",50)
-
-
-console.log(productoMang.getProducts())
+console.log(productoMang.getProducts());
